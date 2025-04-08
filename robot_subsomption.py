d584bad4ca3ed6622a9045e2662f2ffecc969e1a
D-mbbi <doelmbbi@gmail.com>
2025-04-08 21:50:47 +0200

    tp1

---
+++ robot_subsomption.py
@@ -0,0 +1,57 @@
+
+from robot import * 
+from random import choice
+
+nb_robots = 0
+debug = True
+
+class Robot_player(Robot):
+
+    team_name = "Braitenberg_avoider"
+    robot_id = -1
+    iteration = 0
+
+    def __init__(self, x_0, y_0, theta_0, name="n/a", team="n/a"):
+        global nb_robots
+        self.robot_id = nb_robots
+        nb_robots+=1
+        super().__init__(x_0, y_0, theta_0, name=name, team=team)
+
+    def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
+
+        sensor_to_wall = []
+        sensor_to_robot = []
+        for i in range (0,8):
+            if  sensor_view[i] == 1:
+                sensor_to_wall.append( sensors[i] )
+                sensor_to_robot.append(1.0)
+            elif  sensor_view[i] == 2:
+                sensor_to_wall.append( 1.0 )
+                sensor_to_robot.append( sensors[i] )
+            else:
+                sensor_to_wall.append(1.0)
+                sensor_to_robot.append(1.0)
+
+        if debug == True:
+            if self.iteration % 100 == 0:
+                print ("Robot",self.robot_id," (team "+str(self.team_name)+")","at step",self.iteration,":")
+                print ("\tsensors (distance, max is 1.0)  =",sensors)
+                print ("\t\tsensors to wall  =",sensor_to_wall)
+                print ("\t\tsensors to robot =",sensor_to_robot)
+                print ("\ttype (0:empty, 1:wall, 2:robot) =",sensor_view)
+                print ("\trobot's name (if relevant)      =",sensor_robot)
+                print ("\trobot's team (if relevant)      =",sensor_team)
+
+        translation = sensors[sensor_front]*0.5 # A MODIFIER
+        rotation_tout_droit = 0
+        rotation_hateWall = (-1)*(1-sensor_to_wall[sensor_front_left]) + (1)*(1-sensor_to_wall[sensor_front_right]) + choice([-1, 1])*(1-sensor_to_wall[sensor_front])
+        rotation_lovebot = (1)*(1-sensor_to_robot[sensor_front_left]) + (-1)*(1-sensor_to_robot[sensor_front_right]) + choice([-1, 1])*(1-sensor_to_robot[sensor_front]) # A MODIFIER
+
+        rotation = rotation_tout_droit
+        if rotation_hateWall:
+            rotation = rotation_hateWall
+        if rotation_lovebot:
+            rotation = rotation_lovebot
+
+        self.iteration = self.iteration + 1        
+        return translation, rotation, False
